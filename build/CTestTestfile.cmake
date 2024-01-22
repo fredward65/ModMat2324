c7d197466cb629cd27718cbc3d9f500f73f51ff0
Freddy Liendo <32233779+fredward65@users.noreply.github.com>
2024-01-22 13:47:28 +0100

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vscode/ModMath2324/src
-# Build directory: /home/vscode/ModMath2324/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("modmat_toolbox")
