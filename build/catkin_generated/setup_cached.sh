dff857fa40d5fbe4ab364c986dc88605512d3407
fredward65 <freddy-jose.liendo-morales@univ-smb.fr>
2024-01-22 12:44:57 +0000

    Update workspace rebuilding

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -8,7 +8,6 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/vscode/ModMath2324/devel:$CMAKE_PREFIX_PATH"
-export LD_LIBRARY_PATH="/home/vscode/ModMath2324/devel/lib:$LD_LIBRARY_PATH"
 export PWD='/home/vscode/ModMath2324/build'
 export ROSLISP_PACKAGE_DIRECTORIES='/home/vscode/ModMath2324/devel/share/common-lisp'
 export ROS_PACKAGE_PATH="/home/vscode/ModMath2324/src:$ROS_PACKAGE_PATH"
--- ws-setup.sh
+++ ws-setup.sh
@@ -1,11 +1,13 @@
-# Create workspace
+# Source ROS setup file
 source /opt/ros/${ROS_DISTRO}/setup.bash
 
 # Install all dependencies
 rosdep install -y --from-path ~/ModMath2324/src
 
 # Create/Update workspace
-cd ~/ModMath2324 && catkin_make
+cd ~/ModMath2324
+rm -rf -v ./build ./devel ./src/CMakeLists.txt
+catkin_make
 
 # Source the package setup file
 echo "source ~/ModMath2324/devel/setup.bash" >> ~/.bashrc
