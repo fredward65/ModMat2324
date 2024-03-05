01f670236d67de5df3eea3984dd76f8b7150fa41
fredward65 <freddy-jose.liendo-morales@univ-smb.fr>
2024-03-05 15:21:54 +0100

    Update double_pendulum_js

--- src/double_pendulum_js/sketch.js
+++ src/double_pendulum_js/sketch.js
@@ -10,6 +10,12 @@ function draw() {
   background(0, 100);
   translate(width/2, height/2);
   stroke(255);
-  line(0, 0, 0, 100)
-  line(0, 100, 100, 200)
+  let x1 = 0;
+  let y1 = 100;
+  let x2 = 50;
+  let y2 = 200;
+  line(0, 0, x1, y1)
+  circle(x1, y1, 20)
+  line(x1, y1, x2, y2)
+  circle(x2, y2, 20)
 }
